667cde987cfbe92c6bf2f8fb1d73d7b95c9b227a
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-01-05 17:06:54 -0700

    Update arsenal_distribution.py

--- arsenal_distribution.py
+++ arsenal_distribution.py
@@ -119,13 +119,13 @@ if pitches_thrown >= pitch_threshold:
                 axs[ax_num-1].set(xlabel='')
 
         for axis in range(len(pitch_list)):
-        axs[axis].set(ylim=(0,max_count*1.025))
-        axs[axis].legend([pitch_list[axis],'Lg. Avg.'], 
-                         edgecolor='#162B50', loc=(0,0.4), fontsize=14)
-        axs[axis].text(9,max_count*0.425,'{:,}\nPitches'.format(plv_df.loc[(plv_df['pitchername']==player) &
-                                                                           (plv_df['year_played']==year) &
-                                                                           (plv_df['pitchtype']==pitch_list[axis])].shape[0]),
-                       ha='center',va='bottom', fontsize=14)
+            axs[axis].set(ylim=(0,max_count*1.025))
+            axs[axis].legend([pitch_list[axis],'Lg. Avg.'], 
+                             edgecolor='#162B50', loc=(0,0.4), fontsize=14)
+            axs[axis].text(9,max_count*0.425,'{:,}\nPitches'.format(plv_df.loc[(plv_df['pitchername']==player) &
+                                                                               (plv_df['year_played']==year) &
+                                                                               (plv_df['pitchtype']==pitch_list[axis])].shape[0]),
+                           ha='center',va='bottom', fontsize=14)
 
         fig.suptitle("{}'s {} PLV Distributions".format(player,year),fontsize=16)
         sns.despine(left=True)
