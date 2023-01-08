d59aa9d7ec13f45b460f68483de979aea529b83d
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-01-08 00:06:22 -0700

    Update PLV_Batter_Metrics.py

--- PLV_Batter_Metrics.py
+++ PLV_Batter_Metrics.py
@@ -75,7 +75,6 @@ for stat in list(stat_names.values()):
     season_df[stat] = round(z_score_scaler(season_df[stat])*2+10,0)*5
     season_df[stat] = season_df[stat].astype('int')
 
-season_df.columns = [textwrap.wrap(x, width=20) for x in season_df.columns]
 st.dataframe(season_df.style.set_table_styles([dict(selector="th",props=[('max-width', '25px')])]))
 
 # st.title("Rolling Ability Charts")
