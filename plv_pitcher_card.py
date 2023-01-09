baaa8c9286ab1a9ecc310d5bc6dc5d8998a5b5a6
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-01-08 23:01:25 -0700

    Update plv_pitcher_card.py

--- plv_pitcher_card.py
+++ plv_pitcher_card.py
@@ -86,9 +86,9 @@ sz_bot = 1.5
 sz_top = 3.5
 
 # PLV Color Norm
-norm = colors.TwoSlopeNorm(vmin=0, 
+norm = colors.TwoSlopeNorm(vmin=0.5, 
                            vcenter=5,
-                           vmax=10)
+                           vmax=9.5)
 
 game_norm = colors.TwoSlopeNorm(vmin=4, 
                                 vcenter=5,
