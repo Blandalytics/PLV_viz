ef03eb7f81018a64b4d1e6ab99d3525e0b5ccd7e
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-01-09 08:35:44 -0700

    Update plv_pitcher_card.py

--- plv_pitcher_card.py
+++ plv_pitcher_card.py
@@ -284,7 +284,7 @@ def plv_card(pitch_threshold=200,scale_val=1.5):
   graph_data['pitch_qual'] = pd.cut(graph_data['PLV_clip'],bins=qual_bins,labels=qual_labels)
 
   for qual in qual_labels:
-    pitch_plot_ax = plt.subplot(grid[y_loc:y_loc+3, x_loc:x_loc+3])
+    pitch_plot_ax = plt.subplot(grid[y_loc:y_loc+3, x_loc:x_loc+2])
     pitch_qual_charts(graph_data,pitch_plot_ax,qual)
     x_loc += 0
     y_loc += 3
