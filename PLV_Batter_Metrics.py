c056368f007edc29c516cc116b24d3f75a04782d
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-01-09 22:41:19 -0700

    Update PLV_Batter_Metrics.py

--- PLV_Batter_Metrics.py
+++ PLV_Batter_Metrics.py
@@ -206,7 +206,7 @@ def rolling_chart():
                                                      rolling_denom[metric]))
     
     if metric in ['Swing Aggression','Contact Ability','Strikezone Judgement']:
-        ax.yaxis.set_major_formatter(ticker.PercentFormatter(100.0))
+        ax.yaxis.set_major_formatter(ticker.PercentFormatter(0.5))
 
     sns.despine()
     st.pyplot(fig)
