ab2340fa874501d40d46747ef61d9126cb4df6b6
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-01-15 13:45:22 -0700

    Update arsenal_distribution.py

--- arsenal_distribution.py
+++ arsenal_distribution.py
@@ -159,7 +159,7 @@ pla_df = pla_data(plv_df, year)
 
 format_cols = ['PLA','FF','SI','SL','CH','CU','FC','FS']
 
-fill_val = pla_df['PLA'].mean()
+fill_val = pla_df[format_cols].max().max()+0.01
 
 def pitchtype_color(s):
     return f"background-color: {marker_colors[s]}" if s in list(marker_colors.keys()) else None
@@ -168,7 +168,8 @@ st.dataframe(pla_df
              .fillna(fill_val)
              .style
              .format(precision=2, thousands=',')
-             .background_gradient(axis=0, cmap="vlag_r", subset=format_cols)
+             .background_gradient(axis=0, vmin=pla_df['PLA'].min(), vmax=pla_df['PLA'].max(),
+                                  cmap="vlag_r", subset=format_cols)
              .applymap(lambda x: 'color: transparent; background-color: transparent' if x==fill_val else '')
              #.applymap_index(pitchtype_color, axis='columns') # Apparently Streamlit doesn't style headers
             )
