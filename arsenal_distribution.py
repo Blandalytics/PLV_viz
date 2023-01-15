6ff4a1398020e699d60c8e79528d0a685803b290
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-01-14 23:31:52 -0700

    Update arsenal_distribution.py

--- arsenal_distribution.py
+++ arsenal_distribution.py
@@ -179,8 +179,9 @@ format_cols = ['PLA','CH','CU','FC','FF','FS','SI','SI']
 min_val = pla_df[format_cols].min().min()
 max_val = pla_df[format_cols].max().max()
 
+pla_df = pla_df.fillna(max_val+1)
+
 st.dataframe(pla_df
-             .fillna(max_val+1)
              .style
              .format(precision=2, thousands=',')
              .background_gradient(axis=None, vmin=0, vmax=max_val, 
