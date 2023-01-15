6b1370580898c9d303a37e3672984870a6444bf2
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-01-14 23:46:05 -0700

    Update arsenal_distribution.py

--- arsenal_distribution.py
+++ arsenal_distribution.py
@@ -182,13 +182,13 @@ max_val = pla_df[format_cols].max().max()
 pla_df = pla_df
 
 st.dataframe(pla_df
-             #.fillna(max_val+1)
+             .fillna(max_val+1)
              .style
              .format(precision=2, thousands=',')
-#              .background_gradient(axis=None, vmin=0, vmax=max_val, 
-#                                   cmap="vlag_r", subset=format_cols
-#                                  )
-             .applymap(lambda x: 'color: transparent; background-color: transparent' if pd.isnull(x) else '')
+             .background_gradient(axis=None, vmin=0, vmax=max_val, 
+                                  cmap="vlag_r", subset=format_cols
+                                 )
+             #.applymap(lambda x: 'color: transparent; background-color: transparent' if pd.isnull(x) else '')
             )
 
 st.title("PLV Distributions")
