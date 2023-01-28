070622caeaa727cbbcf9ccd8d8b1a1fef28a5d0c
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-01-27 23:09:33 -0700

    Update PLV_Batter_Metrics.py

--- PLV_Batter_Metrics.py
+++ PLV_Batter_Metrics.py
@@ -36,7 +36,7 @@ st.write('- ***Swing Aggression***: How much more often a hitter swings at pitch
 st.write('''
 - ***Strikezone Judgement***: The "correctness" of a hitter's swings and takes, using the likelihood of a pitch being a called strike (for swings) or a ball/HBP (for takes).
 ''')
-st.write("- ***Decision Value***: Modeled value of a hitter's decision to swing or take, minus the modeled value of the alternative. In runs per 100 pitches.")
+st.write("- ***Decision Value***: Modeled value (runs per 100 pitches) of a hitter's decision to swing or take, minus the modeled value of the alternative.")
 st.write("- ***Contact Ability***: A hitter's ability to make contact (foul strike or BIP), above the contact expectation of each pitch.")
 st.write("- ***Power***: Modeled number of extra bases (xISO on contact) above a pitch's expectation, for each BBE.")
 st.write("- ***Hitter Performance (HP)***: Runs added per 100 pitches seen by the hitter (including swing/take decisions), after accounting for pitch quality.")
