05c093ae977078cbe8a2a9dc5d1441a1f6edd067
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-02-09 18:48:54 -0700

    Update PLV_Batter_Metrics.py

--- PLV_Batter_Metrics.py
+++ PLV_Batter_Metrics.py
@@ -353,10 +353,10 @@ def rolling_chart():
         #ax.yaxis.set_major_formatter(ticker.PercentFormatter())
         ax.set_yticklabels([f'{int(x)}%' for x in ax.get_yticks()])
     
-    fig.suptitle("{}'s {} Rolling {}\n{}".format(player,
+    fig.suptitle("{}'s {} {}\n{}".format(player,
                                                  year,
                                                  metric,
-                                                 '({} {}{}{})'.format(window,
+                                                 '(Rolling {} {}{}{})'.format(window,
                                                                       rolling_denom[metric],
                                                                       '' if (count_select in ['All','Custom']) else f'; in {count_select} Counts',
                                                                       '' if (handedness=='All') else f'; {hitter_hand[0]}HH vs {hand_map[handedness][0]}HP'
