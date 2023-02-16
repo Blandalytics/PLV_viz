0a8a8895127a5a1e4bd92167a8973c1005838694
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-02-15 18:17:34 -0700

    Update PLV_Pitcher_Metrics.py

--- PLV_Pitcher_Metrics.py
+++ PLV_Pitcher_Metrics.py
@@ -70,7 +70,7 @@ year = st.radio('Choose a year:', years)
 
 seasonal_constants = pd.read_csv('https://github.com/Blandalytics/PLV_viz/blob/main/data/plv_seasonal_constants.csv?raw=true').set_index('year')
 
-@st.cache
+@st.cache_data
 # Load Data
 def load_data(year):
     df = pd.DataFrame()
