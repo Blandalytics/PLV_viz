3375a2e32ca0b5ebf12c8f7df63b8ce9669c2d43
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-04-10 13:10:02 -0600

    Update PLV_Batter_Metrics.py

--- PLV_Batter_Metrics.py
+++ PLV_Batter_Metrics.py
@@ -212,7 +212,7 @@ else:
                                        ['0-0', '1-0', '2-0', '3-0', '0-1', '1-1', '2-1', '3-1', '0-2', '1-2', '2-2', '3-2'],
                                        ['0-0', '1-0', '2-0', '3-0', '0-1', '1-1', '2-1', '3-1', '0-2', '1-2', '2-2', '3-2'])
     
-updated_threshold = max(50,int(round(rolling_threshold[metric]*len(selected_options)/12/5)*5))
+updated_threshold = max(10 if year == 2023 else 50,int(round(rolling_threshold[metric]*len(selected_options)/12/5)*5))
 
 # Hitter Handedness
 handedness = st.select_slider(
