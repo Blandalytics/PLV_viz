76738b24e17ecb3977603c5aeaef5154cbb30f2f
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-04-17 12:37:47 -0600

    Update PLV_Batter_Metrics.py

--- PLV_Batter_Metrics.py
+++ PLV_Batter_Metrics.py
@@ -307,7 +307,8 @@ def rolling_chart():
                color=sns.color_palette('vlag', n_colors=100)[79],
                linestyle='--',
                alpha=0.5)
-    ax.axhline(0 if (metric in ['Swing Aggression','Contact Ability']) and (count_select=='All') else chart_mean,
+    ax.axhline(#0 if (metric in ['Swing Aggression','Contact Ability']) and (count_select=='All') else chart_mean,
+               chart_mean,
                color='w',
                alpha=0.5)
     ax.axhline(chart_25,
@@ -331,7 +332,8 @@ def rolling_chart():
             color=sns.color_palette('vlag', n_colors=100)[74],
             alpha=1)
     ax.text(line_text_loc,
-            0 if (metric in ['Swing Aggression','Contact Ability']) and (count_select=='All') else chart_mean,
+#             0 if (metric in ['Swing Aggression','Contact Ability']) and (count_select=='All') else chart_mean,
+            chart_mean,
             'MLB Avg' if abs(chart_mean - rolling_df[metric].mean()) > (ax.get_ylim()[1] - ax.get_ylim()[0])/25 else '',
             va='center',
             color='w',
