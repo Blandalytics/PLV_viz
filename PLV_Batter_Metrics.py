eb1bbf3aaa887792430afc809d28b5bf421e2ded
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-04-17 12:33:25 -0600

    Update PLV_Batter_Metrics.py

--- PLV_Batter_Metrics.py
+++ PLV_Batter_Metrics.py
@@ -233,17 +233,17 @@ hand_map = {
 
 st.dataframe(plv_df)
 chart_thresh_list = (plv_df
-#                      .loc[plv_df['count'].astype('str').isin(selected_options) &
-#                           plv_df['b_hand'].isin(hitter_hand) &
-#                           plv_df['p_hand'].isin(hand_map[handedness])
-#                          ]
+                     .loc[plv_df['count'].astype('str').isin(selected_options) &
+                          plv_df['b_hand'].isin(hitter_hand) &
+                          plv_df['p_hand'].isin(hand_map[handedness])
+                         ]
                      .groupby('hittername')
                      [['pitch_id',metric]]
                      .agg({
                          'pitch_id':'count',
                          metric:'mean'
                      })
-#                      .query(f'pitch_id >= {updated_threshold}')
+                     .query(f'pitch_id >= {updated_threshold}')
                      .copy()
                     )
 
