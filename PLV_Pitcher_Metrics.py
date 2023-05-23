03d74b6bd7c68ad4c9d51402089b319a4d21a28e
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-05-23 11:34:24 -0600

    Update PLV_Pitcher_Metrics.py

--- PLV_Pitcher_Metrics.py
+++ PLV_Pitcher_Metrics.py
@@ -89,7 +89,7 @@ seasonal_constants = pd.read_csv('https://github.com/Blandalytics/PLV_viz/blob/m
 @st.cache_data
 def load_data(year):
     df = pd.DataFrame()
-    for month in rnage(3,11):
+    for month in range(3,11):
         file_name = f'https://github.com/Blandalytics/PLV_viz/blob/main/data/{year}_PLV_App_Data-{month}.parquet?raw=true'
         df = pd.concat([df,
                         pd.read_parquet(file_name)[['pitchername','pitcher_mlb_id','pitch_id',
