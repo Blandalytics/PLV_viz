0d4a845f0d33194d11f937083f2ecc1b2545d774
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-06-13 00:29:30 -0600

    Update weighted_offense.py

--- weighted_offense.py
+++ weighted_offense.py
@@ -21,7 +21,7 @@ def highlight_cols(x):
     #return color df
     return df
   
-rank_df = pd.read_csv('https://docs.google.com/spreadsheets/d/1-vizwKykEEPNhUl9mtSR_2VaTslTXVjOLsHqxo3Jpfs/export?format=csv&gid=1365643765')[['Team','wOBA','Tier']].dropna()
+rank_df = pd.read_csv('https://docs.google.com/spreadsheets/d/1-vizwKykEEPNhUl9mtSR_2VaTslTXVjOLsHqxo3Jpfs/export?format=csv&gid=1365643765')[['Team','wOBA','Tier']].query("Tier != ''")
 st.dataframe(rank_df
              .style
              .format(precision=3)
