8ed1abb995f7c626b92a4e65de4b95c53d1d8fc6
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-06-12 23:36:22 -0600

    Create weighted_offense.py

---
+++ weighted_offense.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+rank_df = pd.read_csv('https://docs.google.com/spreadsheets/d/1-vizwKykEEPNhUl9mtSR_2VaTslTXVjOLsHqxo3Jpfs/export?format=csv&gid=1365643765')[['Team','wOBA','Tier']]
+st.dataframe(rank_df)
