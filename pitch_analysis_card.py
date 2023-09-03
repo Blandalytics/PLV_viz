400f2612e13358e9b5ab4109524da8e154c80be8
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-09-03 10:59:16 -0600

    Update pitch_analysis_card.py

--- pitch_analysis_card.py
+++ pitch_analysis_card.py
@@ -125,8 +125,8 @@ with col1:
     # Player
     default_ix = pitcher_list.index('Zack Wheeler')
     card_player = st.selectbox('Choose a player:', pitcher_list, index=default_ix)
+    pitches = pitch_df.loc[pitch_df['pitchername']==card_player,'pitchtype'].map(pitch_names).value_counts().reset_index().query('pitchtype >= 50')['index'].to_list()
 
-pitches = pitch_df.loc[pitch_df['pitchername']==card_player,'pitchtype'].map(pitch_names).value_counts().reset_index().query('pitchtype >= 50')['index'].to_list()
 with col2:
     # Pitch
     pitch_type = st.selectbox('Choose a pitch:', pitches)
