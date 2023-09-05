5ca89dea59fd380e79dadd97f908a680c40c6c5b
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-09-05 01:38:32 -0600

    Update weighted_offense.py

--- weighted_offense.py
+++ weighted_offense.py
@@ -21,10 +21,10 @@ def highlight_cols(x):
     #return color df
     return df
 
-
+pa_df = pd.read_csv('https://github.com/Blandalytics/PLV_viz/blob/main/data/2023_PAs.csv?raw=true')
+  
 @st.cache_data(ttl=12*3600)
 def calc_wOBA_ranks(df=pa_df,time_frame='Season',thresh=0.075):
-    pa_df = pd.read_csv('https://github.com/Blandalytics/PLV_viz/blob/main/data/2023_PAs.csv?raw=true')
     if time_frame=='Season':
         time_thresh = 365
     else:
