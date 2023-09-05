138c1f9a63058ee2f3794bc8d7fe4a379604d06d
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-09-05 01:42:52 -0600

    Update weighted_offense.py

--- weighted_offense.py
+++ weighted_offense.py
@@ -13,6 +13,40 @@ colors = {
   'Poor':['white','#5a84bd']
 }
 
+team_map = {
+    'Arizona Diamondbacks':'ARI', 
+    'Atlanta Braves':'ATL',
+    'Baltimore Orioles':'BAL', 
+    'Boston Red Sox':'BOS', 
+    'Chicago Cubs':'CHC', 
+    'Chicago White Sox':'CHW', 
+    'Cincinnati Reds':'CIN', 
+    'Cleveland Guardians':'CLE', 
+    'Cleveland Indians':'CLE', 
+    'Colorado Rockies':'COL', 
+    'Detroit Tigers':'DET', 
+    'Houston Astros':'HOU', 
+    'Kansas City Royals':'KCR', 
+    'Los Angeles Angels':'LAA', 
+    'Los Angeles Dodgers':'LAD', 
+    'Miami Marlins':'MIA', 
+    'Milwaukee Brewers':'MIL', 
+    'Minnesota Twins':'MIN', 
+    'New York Mets':'NYM',
+    'New York Yankees':'NYY',
+    'Oakland Athletics':'OAK',
+    'Philadelphia Phillies':'PHI',
+    'Pittsburgh Pirates':'PIT',
+    'San Diego Padres':'SDP',
+    'San Francisco Giants':'SFG',
+    'Seattle Mariners':'SEA',
+    'St. Louis Cardinals':'STL',
+    'Tampa Bay Rays':'TBR',
+    'Texas Rangers':'TEX',
+    'Toronto Blue Jays':'TOR',
+    'Washington Nationals':'WSH'
+}
+
 def highlight_cols(x):
     df = x.copy()
     #select all values
