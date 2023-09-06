ad13a0fdb029d3b426b6c56c749b06217c8fae01
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-09-05 18:07:21 -0600

    Update weighted_offense.py

--- weighted_offense.py
+++ weighted_offense.py
@@ -106,7 +106,7 @@ rank_df = calc_wOBA_ranks(df=time_df,time_frame=time_string,thresh=0.075)
 
 st.dataframe(rank_df
              .style
-             .set_properties(**{'background-color': '#eda1a1'}, subset='Top'),
+             .set_properties(**{'background-color': sns.color_palette('spectral', n_colors=101)[0]}, subset='Top'),
              # .format(precision=4)
              # .background_gradient(axis=0,gmap=(rank_df['wOBA']-time_df['wOBA'].mean())/time_df.groupby('hitterteam')['wOBA'].mean().std(), 
              #                      vmin=-2,vmax=2.5,
