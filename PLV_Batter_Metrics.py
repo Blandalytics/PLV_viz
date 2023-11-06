f222abbc8596047632505d9cf2e735f74e0fc0f0
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-11-06 13:12:55 -0700

    Update PLV_Batter_Metrics.py

--- PLV_Batter_Metrics.py
+++ PLV_Batter_Metrics.py
@@ -178,7 +178,11 @@ with col1:
 
 with col2:
     # Pitchtype Selection
-    pitchtype_help = '***Fastballs***: 4-Seam, Sinkers, some Cutters\n***Breaking Balls***: Sliders, Curveballs, most Cutters\n***Offspeed***: Changeups, Splitters'
+    pitchtype_help = '''
+    *Fastballs*: 4-Seam, Sinkers, some Cutters\n
+    *Breaking Balls*: Sliders, Curveballs, most Cutters\n
+    *Offspeed*: Changeups, Splitters
+    '''
     pitchtype_base = st.selectbox('Vs Pitchtype', 
                                   ['All','Fastball', 'Breaking Ball', 'Offspeed'],
                                   index=0,
