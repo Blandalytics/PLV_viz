d5567e4187d3c5d134268beed6ed6f23b4ecc9a7
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-12-23 13:27:36 -0700

    Update smoothing_code_2d.py

--- smoothing_code_2d.py
+++ smoothing_code_2d.py
@@ -4,6 +4,7 @@ import seaborn as sns
 
 from statsmodels.nonparametric.kernel_regression import KernelReg
 
+### Dataframe with 1000 observations, using normally distributed X/Z coords and random stat values
 df_size = 1000
 
 test_data = {
@@ -13,15 +14,24 @@ test_data = {
 }
 test_df = pd.DataFrame(test_data)
 
+### Smooth the stat values
+# Specify the bandwidth.
+# I eyeballed the value for my stats, but I'm sure there's plenty of literature on it
 bandwidth = 1
 
+# Train the kernel regression model on the location and stat values, using the provided bandwidth
 kernel_regression = KernelReg(endog=test_df['test_stat'], 
                               exog= [test_df['x'], test_df['z']], 
                               bw=[bandwidth,bandwidth],
-                              var_type='cc')
+                              var_type='cc') # States that X & Z are both continuous (c)
 
+# Apply the model onto the X/Z coords
 test_df['smoothed_stat'] = kernel_regression.fit([test_df['x'], test_df['z']])[0]
+
+### Generate viz
+# Pivot df to 2D, to better play with Seaborn's heatmap code 
 heatmap_df = test_df.pivot_table(columns='x',index='z',values=['smoothed_stat'], aggfunc='mean')
 
+# Generate Chart
 sns.heatmap(data=heatmap_df['smoothed_stat'].astype('float'))
 sns.despine()
