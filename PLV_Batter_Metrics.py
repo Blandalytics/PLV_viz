04560f8cd49e35bf6627dba9cd9f57441eda4adc
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-12-25 17:59:46 -0700

    Update PLV_Batter_Metrics.py

--- PLV_Batter_Metrics.py
+++ PLV_Batter_Metrics.py
@@ -93,7 +93,7 @@ def load_season_data(year):
     
     df = df.reset_index(drop=True)
 
-    print(df['p_z'].mean())
+    st.write(df['p_z'].mean())
 
     df.loc[df['p_x'].notna(),'kde_x'] = np.clip(df.loc[df['p_x'].notna(),'p_x'].astype('float').mul(12).round(0).astype('int').div(12),
                                                         -20/12,
