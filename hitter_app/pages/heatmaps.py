5a39d51d89b8b65ff64e90800fe28e250d81c3b0
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-01-03 09:45:26 -0700

    Update heatmaps.py

--- hitter_app/pages/heatmaps.py
+++ hitter_app/pages/heatmaps.py
@@ -13,7 +13,7 @@ from PIL import Image
 from scipy import stats
 from statsmodels.nonparametric.kernel_regression import KernelReg
 
-logo_loc = 'https://github.com/Blandalytics/PLV_viz/blob/main/data/PL-text-wht.png?raw=true'
+logo_loc = 'PL-text-wht.png?raw=true'
 logo = Image.open(urllib.request.urlopen(logo_loc))
 st.image(logo, width=200)
 
