1540da3a5d933ef331e156fa1ac75378c87a5b2b
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-03-15 14:44:08 -0600

    Update plv_stuff_app.py

--- plv_stuff_app.py
+++ plv_stuff_app.py
@@ -104,11 +104,7 @@ player = st.selectbox('Choose a player:', players, index=default_ix)
 
 hand = 'L' if year_data.loc[(year_data['pitchername']==player),'pitcherside_L'].values[0] == 1 else 'R'
 
-st.write('Controls:')
-st.write('- Hover to see pitch details')
-st.write('- Left click drag to rotate the chart')
-st.write('- Scroll to zoom')
-st.write('- Right click drag to move the chart')
+st.write('Controls:\n- Hover to see pitch details\n- Left click drag to rotate the chart\n- Scroll to zoom\n- Right click drag to move the chart')
 
 def stuff_chart(df,player):
     chart_df = df.loc[(df['pitchername']==player)].copy()
