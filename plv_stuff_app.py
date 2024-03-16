9a8e53aabfaf4e4d85955d61ac55f9628830865d
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-03-16 11:25:58 -0600

    Update plv_stuff_app.py

--- plv_stuff_app.py
+++ plv_stuff_app.py
@@ -83,12 +83,36 @@ pitch_threshold = st.number_input(f'Min # of Pitches:',
                                   step=50, 
                                   value=500 if year != 2024 else 0)
 
-@st.cache_data
+@st.cache_data(ttl=2*3600,show_spinner=f"Loading {year} data")
 def load_data(year):
     file_name = f'https://github.com/Blandalytics/PLV_viz/blob/main/data/{year}_PLV_Stuff_App_Data.parquet?raw=true'
     return pd.read_parquet(file_name)
 
 year_data = load_data(year)
+
+st.dataframe(pitch_order = ['FF','SI','FC','SL','ST','CU','CH','FS']
+    (pd.pivot_table((year_data
+                     .loc[~(year_data['pitchtype'].isin(['KN','SC','UN'])) & 
+                          (year_data['pitch_id'].groupby([year_data['pitchername'],year_data['pitchtype']]).transform('count')>=10)]), 
+                   values=['plv_stuff_plus','pitch_id'], index=['pitchername'],
+                   columns='pitchtype', aggfunc={'plv_stuff_plus':'mean','pitch_id':'count'})
+     .assign(Num_Pitches = lambda x: x[[('pitch_id',y) for y in pitch_order]].sum(axis=1),
+             plvStuff = lambda x: x[[('plv_stuff_plus',y) for y in pitch_order]].mul(x[[('pitch_id',y) for y in pitch_order]].droplevel(0, axis=1)).sum(axis=1) / x['Num_Pitches'])
+     .drop(columns=[('pitch_id',y) for y in pitch_order+['KN','SC','UN']])
+     .droplevel(0, axis=1)
+     .reset_index()
+     .set_axis(['Pitcher','CH','CU','FC','FF','FS','SI','SL','ST','Pitches','plvStuff+'], axis=1)
+     .set_index('Pitcher')
+     [['Pitches','plvStuff+']+pitch_order]
+     .query(f'Pitches >= {pitch_threshold}')
+     .sort_values('plvStuff+',ascending=False)
+     .fillna(-100)
+     .style
+     .format(precision=1, thousands=',')
+     .background_gradient(axis=0, vmin=50, vmax=150,
+                          cmap="vlag", subset=['plvStuff+']+pitch_order)
+     .map(lambda x: 'color: transparent; background-color: transparent' if x==-100 else '')
+                              )
 
 players = list(year_data
                .groupby('pitchername')
