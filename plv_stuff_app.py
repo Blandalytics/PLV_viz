1f9f2b2cb7874bf27076ccf9a4a7d24f9986c041
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-03-20 10:44:01 -0600

    Update plv_stuff_app.py

--- plv_stuff_app.py
+++ plv_stuff_app.py
@@ -225,13 +225,14 @@ def stuff_chart(df,player,palette):
         bonus_text = chart_df['pitchtype'].map(pitch_names)
         hover_text = '<b>%{text}</b><br><b>plvStuff+: %{marker.color:.1f}</b><br>Velo: %{y}mph<br>IVB: %{z:.1f}"<br>Arm-Side Break: %{x:.1f}"<extra></extra>'
     else:
-        labels = chart_df['pitchtype']
-        marker_dict = dict(color=labels.map(marker_colors),size=5,line=dict(width=0.25,color=pl_line_color))
+        labels = chart_df['pitchtype'].map(marker_colors)
+        marker_dict = dict(color=labels,size=5,line=dict(width=0.25,color=pl_line_color))
         bonus_text = chart_df['3d_stuff_plus']
-        hover_text = '<b>%{labels}</b><br><b>plvStuff+: %{text:.1f}</b><br>Velo: %{y}mph<br>IVB: %{z:.1f}"<br>Arm-Side Break: %{x:.1f}"<extra></extra>'
+        hover_text = '<b>%{customdata}</b><br><b>plvStuff+: %{text:.1f}</b><br>Velo: %{y}mph<br>IVB: %{z:.1f}"<br>Arm-Side Break: %{x:.1f}"<extra></extra>'
     trace = go.Scatter3d(x=chart_df['IHB'].mul(-1 if hand=='R' else 1), y=chart_df['velo'], z=chart_df['IVB'], 
                          mode='markers', marker=marker_dict,
                          text=bonus_text,
+                         customdata=chart_df['pitchtype'],
                          hovertemplate=hover_text
                          )
     layout = go.Layout(margin=dict(l=30,r=0,t=45, b=30
