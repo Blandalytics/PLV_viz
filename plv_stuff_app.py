32b404d5a740322af7fa58c41b96671b7a04c31f
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-03-22 11:48:00 -0600

    Update plv_stuff_app.py

--- plv_stuff_app.py
+++ plv_stuff_app.py
@@ -144,15 +144,15 @@ st.write(f"{player}'s Repertoire")
 st.dataframe(year_data
              .loc[(year_data['pitchername']==player)]
              .groupby('pitchtype')
-             [['pitch_id','velo','IVB','IHB','swinging_strike_pred','plv_stuff_plus','wOBAcon_pred','in_play_input']]
+             [['pitch_id','velo','IVB','IHB','swinging_strike_pred','plv_stuff_plus','adj_vaa','pitch_extension']]
              .agg({
                  'pitch_id':'count',
                  'velo':'mean',
                  'IVB':'mean',
                  'IHB':'mean',
                  'swinging_strike_pred':'mean',
-                 'wOBAcon_pred':'sum',
-                 'in_play_input':'sum',
+                 'adj_vaa':'mean',
+                 'pitch_extension':'mean',
                  'plv_stuff_plus':'mean'
                  })
              .astype({
@@ -161,31 +161,40 @@ st.dataframe(year_data
                  'IVB':'float',
                  'IHB':'float',
                  'swinging_strike_pred':'float',
-                 'wOBAcon_pred':'float',
-                 'in_play_input':'float',
+                 'adj_vaa':'float',
+                 'pitch_extension':'float',
                  'plv_stuff_plus':'float'
                  })
              .reset_index()
              .assign(pitchtype = lambda x: x['pitchtype'].map(pitch_names),
-                     IHB = lambda x: x['IHB'].mul(-1 if hand=='R' else 1),
-                     wOBAcon = lambda x: x['wOBAcon_pred'].div(x['in_play_input'])
+                     IHB = lambda x: x['IHB'].mul(-1 if hand=='R' else 1)
                     )
              .rename(columns={
                  'pitchtype':'Pitch Type',
                  'pitch_id':'Pitches',
+                 'pitch_extension':'Extension',
                  'velo':'Velo (mph)',
                  'IHB':'Arm-Side Break',
                  'swinging_strike_pred':'xWhiff%',
-                 'wOBAcon':'stuff wOBAcon',
+                 'adj_vaa':'HAVAA',
                  'plv_stuff_plus':'plvStuff+'
                  })
              .set_index('Pitch Type')
              .dropna()
              .sort_values('Pitches',ascending=False)
              .reset_index()
-             [['Pitch Type','Pitches','Velo (mph)','IVB','Arm-Side Break','xWhiff%','stuff wOBAcon','plvStuff+']]
+             [['Pitch Type','Pitches','Extension','Velo (mph)','IVB','Arm-Side Break','HAVAA','xWhiff%','plvStuff+']]
              .style
-             .format({'Pitches':'{:,.0f}', 'Velo (mph)':'{:.1f}', 'IVB': '{:.1f}"', 'Arm-Side Break': '{:.1f}"', 'xWhiff%':'{:.1%}', 'stuff wOBAcon':'{:.3f}', 'plvStuff+': '{:.1f}'})
+             .format({
+                 'Pitches':'{:,.0f}', 
+                 'Extension':'{:.1f}ft', 
+                 'Velo (mph)':'{:.1f}', 
+                 'IVB': '{:.1f}"', 
+                 'Arm-Side Break': '{:.1f}"', 
+                 'HAVAA':'{:.1f}°', 
+                 'xWhiff%':'{:.1%}', 
+                 'plvStuff+': '{:.1f}'
+             })
              .background_gradient(axis=0, vmin=50, vmax=150,
                                   cmap="vlag", subset=['plvStuff+']),
              hide_index=True
