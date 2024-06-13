615acc194aa6cf9f73f805f04263b1899168c531
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-06-13 13:58:45 -0600

    Update pitch_level_plv.py

--- pitch_level_plv.py
+++ pitch_level_plv.py
@@ -211,6 +211,7 @@ sns.kdeplot((year_data
             hue='Type',
             palette=marker_colors,
             linewidth=2,
+            multiple="stack",
             cut=0
            )
 fig.suptitle(f'MLB {szn_metric_title} Distribution')
