d597610ba83e1afb35011b5b99d7b2a0cc298c90
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-06-13 13:41:01 -0600

    Update pitch_level_plv.py

--- pitch_level_plv.py
+++ pitch_level_plv.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import urllib
-import matplotlib.pypot as plt
+import matplotlib.pyplot as plt
 
 from datetime import datetime
 from PIL import Image
