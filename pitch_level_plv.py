c4da29c9fafb45d3a5f25c7439aa82ae5e153831
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-06-17 13:39:17 -0600

    Update pitch_level_plv.py

--- pitch_level_plv.py
+++ pitch_level_plv.py
@@ -123,6 +123,8 @@ def load_data(year):
     df = pd.DataFrame()
     for month in range(3,11):
         file_name = f'https://github.com/Blandalytics/PLV_viz/blob/main/data/{year}_PLV_Stuff_App_Data-{month}.parquet?raw=true'
+        if pd.read_parquet(file_name).shape[0]==0:
+            continue
         df = pd.concat([df,
                         pd.read_parquet(file_name)], ignore_index=True)
     return df.query('pitchtype != "UN"').reset_index(drop=True)
