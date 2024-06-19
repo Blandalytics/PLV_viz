59a17e4b010d63ff4be9052f30b30a4f3a53c431
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-06-19 11:52:11 -0600

    Update pitch_level_plv.py

--- pitch_level_plv.py
+++ pitch_level_plv.py
@@ -280,7 +280,7 @@ st.dataframe(pd.pivot_table((year_data
             )
 
 st.header('Pitch-Level Metrics')
-games = list(year_data.loc[year_data['pitchername']==player,'game_played'].unique())
+games = list(year_data.loc[year_data['pitchername']==player,'game_played'].sort_values('game_played').unique())
 game_date = st.selectbox('Choose a game:', games, index=len(games)-1)
 # date_format = '%Y-%m-%d'
 # game_date = datetime.strptime(game_date_str, date_format).date()
