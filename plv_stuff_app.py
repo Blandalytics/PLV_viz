42e05e307cef0beed1e8b4b8eac6047c2a80707e
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2024-06-28 09:53:30 -0600

    Update plv_stuff_app.py

--- plv_stuff_app.py
+++ plv_stuff_app.py
@@ -293,7 +293,7 @@ def stuff_chart(df,player,palette):
         labels = chart_df['pitchtype'].map(marker_colors)
         marker_dict = dict(color=labels,size=5,line=dict(width=0.25,color=pl_line_color))
         bonus_text = chart_df['3d_stuff_plus']
-        hover_text = '<b>%{customdata}</b><br><b>plvStuff+: %{text:.1f}</b><br>Velo: %{y}mph<br>IVB: %{z:.1f}"<br>Arm-Side Break: %{x:.1f}"<extra></extra>'
+        hover_text = '<b>%{customdata}</b><br><b>%{palette}: %{text:.1f}</b><br>Velo: %{y}mph<br>IVB: %{z:.1f}"<br>Arm-Side Break: %{x:.1f}"<extra></extra>'
     trace = go.Scatter3d(x=chart_df['IHB'].mul(-1 if hand=='R' else 1), y=chart_df['velo'], z=chart_df['IVB'], 
                          mode='markers', marker=marker_dict,
                          text=bonus_text,
