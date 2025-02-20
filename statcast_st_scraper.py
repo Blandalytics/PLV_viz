1b9d14c832c2ab22ccf50fd0785d97ddc227fdad
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-02-20 09:27:18 -0800

    Update statcast_st_scraper.py

--- statcast_st_scraper.py
+++ statcast_st_scraper.py
@@ -170,6 +170,7 @@ def scrape_savant_data(player_name, game_id):
     merge_df['CSW'] = [f'{x:.1f}%' for x in merge_df['CSW']]
     merge_df['vs R'] = [f'{x:.1%}' for x in merge_df['vs_rhh']]
     merge_df['vs L'] = [f'{x:.1%}' for x in merge_df['vs_lhh']]
+    merge_df['Ext'] = merge_df['Ext'].round(1)
 
     merge_df['Usage'] = np.where(merge_df['Usage Diff'].isna(),
                                  [f'{x:.1f}%' for x in merge_df['Usage']],
@@ -183,7 +184,7 @@ def scrape_savant_data(player_name, game_id):
     merge_df['IHB'] = np.where(merge_df['IHB Diff'].isna(),
                                  [f'{x:.1f}"' for x in merge_df['IHB']],
                                  [f'{x:.1f}" ({y:+.1f}")' for x,y in zip(merge_df['IHB'],merge_df['IHB Diff'].fillna(0))])
-    
+     
     # if df.loc[df['Pitcher']==player_name,'MLBAMID'].unique()[0] in list(season_avgs['pitcher']):
     #     merge_df['Usage'] = [f'{x:.1f}% ({y:+.1f}%)' for x,y in zip(merge_df['Usage'],merge_df['Usage Diff'].fillna(0))]
     #     merge_df['Velo'] = [f'{x:.1f} ({y:+.1f})' for x,y in zip(merge_df['Velo'],merge_df['Velo Diff'].fillna(0))]
