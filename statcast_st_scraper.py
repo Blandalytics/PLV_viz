94a5bed73d58142ddf9d039cc4305cde98b10d94
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-02-23 11:14:14 -0800

    Update statcast_st_scraper.py

--- statcast_st_scraper.py
+++ statcast_st_scraper.py
@@ -224,12 +224,13 @@ elif st.button("Generate Player Table"):
     table_df = scrape_savant_data(player_select,game_id)
     st.dataframe(table_df,#.style.background_gradient(axis=0, vmin=0, vmax=0.755,cmap="vlag_r", subset=['3D wOBAcon']),
                  column_config={
-                     "Num Pitches": st.column_config.NumberColumn(
-                         "#",
-                         width="small",
-                         ),
+                     # "Num Pitches": st.column_config.NumberColumn(
+                     #     "#",
+                     #     width="small",
+                     #     ),
                      "3D wOBAcon": st.column_config.NumberColumn(
                          help="xwOBA on contact, using Launch Speed, Launch Angle, and Spray Angle",
                          ),
                      },
-                 use_container_width=True,hide_index=True)
+                 # use_container_width=True,
+                 hide_index=True)
