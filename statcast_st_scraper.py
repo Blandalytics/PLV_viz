ace4dadea3218358b7ac7b31bf173fdc5a3b6af0
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-02-24 11:35:20 -0800

    Update statcast_st_scraper.py

--- statcast_st_scraper.py
+++ statcast_st_scraper.py
@@ -291,8 +291,8 @@ elif st.button("Generate Player Table"):
                      "HAVAA": st.column_config.Column(
                          help="""
                          Height-Adjusted Vertical Approach Angle
-                          + means flatter than pitches at that location
-                          - means steeper than pitches at that location
+                         >0 means flatter than other pitches at that location
+                         <0 means steeper than pitches at that location
                          """,
                          ),
                      "vs R": st.column_config.Column(
