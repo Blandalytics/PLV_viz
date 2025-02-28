65936e1f2044b531decc41378279a4a64772938a
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-02-28 13:59:51 -0800

    Update auction_calc.py

--- auction_calc.py
+++ auction_calc.py
@@ -27,7 +27,8 @@ with col4:
 
 col1, col2, col3, col4 = st.columns(4)
 with col3:
-    bench_suppress = st.checkbox("Minimize bench value",value=True)
+    bench_suppress = st.checkbox("Minimize bench value",value=True,
+                                 help='Does not consider bench players when calculating replacement level')
     num_bench = 0 if bench_suppress else num_bench
 
 
