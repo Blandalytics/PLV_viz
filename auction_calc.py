84a92e513e519bde0a3a58dbe77125df51fe1b88
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-02-28 13:41:17 -0800

    Update auction_calc.py

--- auction_calc.py
+++ auction_calc.py
@@ -23,7 +23,7 @@ with col2:
 with col3:
     team_budget = st.number_input('Per-Team Budget:',min_value=(min_bid+1)*(num_hitters+num_pitchers+num_bench),value=260)
 with col4:
-    hitter_split = st.number_input('Hitter Split of budget (%):',min_value=0.,max_value=100.,value=65, format="%0.1f")
+    hitter_split = st.number_input('Hitter Split of budget (%):',min_value=0.,max_value=100.,value=65.0, format="%0.1f")
     hitter_split = hitter_split/100
 
 st.header('Scoring Categories')
