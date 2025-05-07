951c125894e09a6680073c2156016e85a9725136
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-05-07 01:21:03 -0700

    Update pl_live_feed.py

--- pl_live_feed.py
+++ pl_live_feed.py
@@ -197,6 +197,7 @@ def generate_games(games_today):
                     game_info = f'FINAL: **:green[{away_team} {away_runs}]** @ {home_team} {home_runs}'
             else:
                 game_info = f'{top_bot}{inning}: {away_team} {away_runs} @ {home_team} {home_runs}'
+        game_info = f'{game_info:<22}'
         game_dict.update({game_info:[game,game_time,raw_time,inning_sort,code_map]})
     game_df = pd.DataFrame.from_dict(game_dict, orient='index',columns=['Game ID','Time','Sort Time','Sort Inning','Sort Code'])
     return game_df.sort_values(['Sort Code','Sort Time','Game ID','Sort Inning'])['Game ID'].to_dict()
