88b6e8d3d26bd96a14175fc5fc22d50190f0ad8b
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-05-08 16:07:25 -0700

    Update pl_live_feed.py

--- pl_live_feed.py
+++ pl_live_feed.py
@@ -280,8 +280,8 @@ with col3:
         hold = stat_base['holds']
         blown_save = stat_base['blownSaves']
         home_away = 'vs' if home==1 else '@'
-        qs  = ', QS' if (int(innings[0])>=6) and (int(earned_runs)<=3) else ''
-        decision = f'(ND{qs})' if (win+loss==0) and (starter==1) else f'(W{qs})' if win==1 else f'(L{qs})' if loss==1 else '(SV)' if save==1 else '(HD)' if hold==1 else '(BS)' if blown_save==1 else ''
+        supplemental_decision  = ', QS' if (int(innings[0])>=6) and (int(earned_runs)<=3) else ', BS' if blown_save==1 else ''
+        decision = f'(ND{supplemental_decision})' if (win+loss==0) and (starter==1) else f'(W{supplemental_decision})' if win==1 else f'(L{supplemental_decision})' if loss==1 else '(SV)' if save==1 else '(HD)' if hold==1 else ''
         decision = decision if game_code == 'F' else ''
     else:
         away_pitcher = x['scoreboard']['probablePitchers']['away']['fullName']
