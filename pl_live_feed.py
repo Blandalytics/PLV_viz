c2429be83f1e86a39e3206dbe89e18385973bdbd
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-05-09 10:32:39 -0600

    Update pl_live_feed.py

--- pl_live_feed.py
+++ pl_live_feed.py
@@ -17,8 +17,6 @@ import xgboost as xgb
 from xgboost import XGBClassifier
 
 from PIL import Image
-
-st.write()
 # Convenience Functions
 def adjusted_vaa(dataframe):
     ## Physical characteristics of pitch
