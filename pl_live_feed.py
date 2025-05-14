6dc745a0331044c68419753fddddf562c3ee031e
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-05-14 11:48:59 -0700

    Update pl_live_feed.py

--- pl_live_feed.py
+++ pl_live_feed.py
@@ -153,8 +153,8 @@ with col2:
             if f'{home_away_pitcher}_pitchers' not in x.keys():
                 continue
             for pitcher_id in list(x[f'{home_away_pitcher}_pitchers'].keys()):
-                test_list.update({x[f'{home_away_pitcher}_pitchers'][pitcher_id][0]['pitcher_name']:pitcher_id})
-        test_list = {v: k for k, v in test_list.items() if int(v) in pitcher_lineup}
+                test_list.update({pitcher_id:x[f'{home_away_pitcher}_pitchers'][pitcher_id][0]['pitcher_name']})
+        pitcher_lineup = [int(x) for x in test_list.keys() if int(x) in pitcher_lineup]
         if len(test_list.keys())>0:
             pitcher_list = {test_list[str(x)]:[str(x),y] for x,y in zip(pitcher_lineup,home_team)}
         else:
