527155347073bd5af01d6fa2c6948240b6cb98d9
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-06-23 20:04:40 -0700

    Update pl_live_feed.py

--- pl_live_feed.py
+++ pl_live_feed.py
@@ -1815,5 +1815,5 @@ def plotly_charts(chart_df):
     # fig.show()
     st.plotly_chart(fig,use_container_width=False,theme=None)
 
-if st.button('Experimental test charts (NOT mobile-friendly)'):
-    plotly_charts(chart_df)
+# if st.button('Experimental test charts (NOT mobile-friendly)'):
+    # plotly_charts(chart_df)
