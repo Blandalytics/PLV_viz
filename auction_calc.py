755bd8f3fd6f1985b2914e16f61f83c842f207be
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-12-12 00:18:17 -0700

    Update auction_calc.py

--- auction_calc.py
+++ auction_calc.py
@@ -372,7 +372,7 @@ if scoring_style=='Categories':
 else:
     combined_value_df['Value'] = combined_value_df[list(point_values.keys())].mul(point_values).sum(axis=1)
 combined_value_df['Rank'] = combined_value_df['Value'].rank(ascending=False)
-display_df = combined_value_df[['Rank','Name','Y! Pos','Value']+[x+'_h' if x in pitcher_cats else x for x in hitter_cats]+[x+'_p' if x in hitter_cats else x for x in pitcher_cats]].sort_values('Auction $',ascending=False).copy()
+display_df = combined_value_df[['Rank','Name','Y! Pos','Value']+[x+'_h' if x in pitcher_cats else x for x in hitter_cats]+[x+'_p' if x in hitter_cats else x for x in pitcher_cats]].sort_values('Value',ascending=False).copy()
 st.dataframe(display_df,
              use_container_width=True,
              hide_index=True,
